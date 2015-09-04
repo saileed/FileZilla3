6983a2b8f432050c747b352340e2fdefdea9f562
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2015-09-04 11:41:03 +0000

    Tag 3.14.0-rc1

---
+++ FileZilla3/tags/3.14.0-rc1/src/include/fzstring.h
@@ -0,0 +1,17 @@
+#ifndef FILEZILLA_ENGINE_FZSTRING_HEADER
+#define FILEZILLA_ENGINE_FZSTRING_HEADER
+
+#include "libfilezilla.h"
+
+#include <string>
+
+typedef std::wstring fzstring;
+
+inline fzstring to_fzstring(wxString const& s) { return s.ToStdWstring(); }
+#ifdef __WXMSW__
+inline int collate_fzstring(fzstring const& a, fzstring const& b) { return _wcsicmp(a.c_str(), b.c_str()); } // note: does not handle embedded null
+#else
+inline int collate_fzstring(fzstring const& a, fzstring const& b) { return wcscasecmp(a.c_str(), b.c_str()); } // note: does not handle embedded null
+#endif
+
+#endif
