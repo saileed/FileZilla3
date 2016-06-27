ae15dc07524901be64ac967479237a2320577365
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2016-06-27 14:19:36 +0000

    Tag 3.19.0

---
+++ FileZilla3/tags/3.19.0/src/interface/buildinfo.h
@@ -0,0 +1,25 @@
+#ifndef __BUILDINFO_H__
+#define __BUILDINFO_H__
+
+class CBuildInfo
+{
+protected:
+	CBuildInfo() {}
+
+public:
+
+	static wxString GetVersion();
+	static int64_t ConvertToVersionNumber(const wxChar* version);
+	static wxString GetBuildDateString();
+	static wxString GetBuildTimeString();
+	static fz::datetime GetBuildDate();
+	static wxString GetBuildType();
+	static wxString GetCompiler();
+	static wxString GetCompilerFlags();
+	static wxString GetHostname();
+	static wxString GetBuildSystem();
+	static bool IsUnstable(); // Returns true on beta or rc releases.
+	static wxString GetCPUCaps(char separator = ',');
+};
+
+#endif //__BUILDINFO_H__
