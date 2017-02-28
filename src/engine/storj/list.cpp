bdcb63022965bab83e1f336353b3d14e7b80b1fe
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2017-02-28 09:18:22 +0000

    Finish refactoring the list command.

--- src/engine/storj/list.cpp
+++ src/engine/storj/list.cpp
@@ -6,7 +6,7 @@
 enum listStates
 {
 	list_init = 0,
-	list_waitcwd,
+	list_waitresolve,
 	list_waitlock,
 	list_list
 };
@@ -15,6 +15,65 @@ int CStorjListOpData::Send()
 {
 	LogMessage(MessageType::Debug_Verbose, L"CStorjListOpData::Send() in state %d", opState);
 
+	switch (opState) {
+	case list_init:
+		if (!subDir_.empty()) {
+			LogMessage(MessageType::Error, _("Invalid path"));
+			return FZ_REPLY_ERROR;
+		}
+
+		if (path_.empty()) {
+			path_ = CServerPath(L"/");
+		}
+
+		currentPath_ = path_;
+
+		if (!currentServer_) {
+			LogMessage(MessageType::Debug_Warning, L"CStorjControlSocket::List called with m_pCurrenServer == 0");
+			return FZ_REPLY_INTERNALERROR;
+		}
+
+		if (currentPath_.GetType() != ServerType::UNIX) {
+			LogMessage(MessageType::Debug_Warning, L"CStorControlSocket::List called with incompatible server type %d in path", currentPath_.GetType());
+			return FZ_REPLY_INTERNALERROR;
+		}
+
+		if (path_.SegmentCount() > 1) {
+			LogMessage(MessageType::Error, _("Invalid path"));
+			return FZ_REPLY_ERROR;
+		}
+
+		opState = list_waitresolve;
+		controlSocket_.Resolve(path_, std::wstring(), bucket_);
+		return FZ_REPLY_CONTINUE;
+	case list_waitlock:
+		if (!holdsLock_) {
+			LogMessage(MessageType::Debug_Warning, L"Not holding the lock as expected");
+			return FZ_REPLY_INTERNALERROR;
+		}
+
+		{
+			// Check if we can use already existing listing
+			CDirectoryListing listing;
+			bool is_outdated = false;
+			bool found = engine_.GetDirectoryCache().Lookup(listing, currentServer_, path_, false, is_outdated);
+			if (found && !is_outdated &&
+				listing.m_firstListTime >= time_before_locking_)
+			{
+				controlSocket_.SendDirectoryListingNotification(listing.path, topLevel_, false);
+				return FZ_REPLY_OK;
+			}
+		}
+		opState = list_list;
+		return FZ_REPLY_CONTINUE;
+	case list_list:
+		if (bucket_.empty()) {
+			return controlSocket_.SendCommand(L"list-buckets");
+		}
+		else {
+			return controlSocket_.SendCommand(L"list " + bucket_);
+		}
+	}
 
 	LogMessage(MessageType::Debug_Warning, L"Unknown opState in CStorjListOpData::ListSend()");
 	return FZ_REPLY_INTERNALERROR;
@@ -24,6 +83,20 @@ int CStorjListOpData::ParseResponse()
 {
 	LogMessage(MessageType::Debug_Verbose, L"CStorjListOpData::ParseResponse() in state %d", opState);
 
+	if (opState == list_list) {
+		if (controlSocket_.result_ != FZ_REPLY_OK) {
+			return controlSocket_.result_;
+		}
+		directoryListing_.path = path_;
+
+		directoryListing_.m_firstListTime = fz::monotonic_clock::now();
+
+		engine_.GetDirectoryCache().Store(directoryListing_, currentServer_);
+		controlSocket_.SendDirectoryListingNotification(directoryListing_.path, topLevel_, false);
+
+		currentPath_ = path_;
+		return FZ_REPLY_OK;
+	}
 
 	LogMessage(MessageType::Debug_Warning, L"ListParseResponse called at inproper time: %d", opState);
 	return FZ_REPLY_INTERNALERROR;
@@ -33,6 +106,21 @@ int CStorjListOpData::SubcommandResult(int prevResult, COpData const& previousOp
 {
 	LogMessage(MessageType::Debug_Verbose, L"CStorjListOpData::SubcommandResult() in state %d", opState);
 
+	if (prevResult != FZ_REPLY_OK) {
+		return prevResult;
+	}
+
+	switch (opState) {
+	case list_waitresolve:
+		opState = list_waitlock;
+		if (!controlSocket_.TryLockCache(CStorjControlSocket::lock_list, path_)) {
+			time_before_locking_ = fz::monotonic_clock::now();
+			return FZ_REPLY_WOULDBLOCK;
+		}
+
+		opState = list_list;
+		return FZ_REPLY_CONTINUE;
+	}
 
 	LogMessage(MessageType::Debug_Warning, L"Unknown opState in CStorjListOpData::SubcommandResult()");
 	return FZ_REPLY_INTERNALERROR;
