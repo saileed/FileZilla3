921b061816e1bd0aaad9fffed23c264140507c6f
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2017-02-28 08:50:30 +0000

    Add operation to get file and bucket ids.

---
+++ src/engine/storj/resolve.cpp
@@ -0,0 +1,100 @@
+#include <filezilla.h>
+
+#include "directorycache.h"
+#include "resolve.h"
+
+enum resolveStates
+{
+	resolve_init = 0,
+	resolve_waitlistbuckets,
+	resolve_id,
+	resolve_waitlist
+};
+
+int CStorjResolveOpData::Send()
+{
+	LogMessage(MessageType::Debug_Verbose, L"CStorjResolveOpData::Send() in state %d", opState);
+
+	switch (opState) {
+	case resolve_init:
+		if (path_.empty()) {
+			return FZ_REPLY_INTERNALERROR;
+		}
+		else if (path_.SegmentCount() != 1) {
+			return FZ_REPLY_INTERNALERROR;
+		}
+		else {
+			CDirectoryListing buckets;
+
+			bool outdated{};
+			bool found = engine_.GetDirectoryCache().Lookup(buckets, currentServer_, CServerPath(L"/"), false, outdated);
+			if (found && !outdated) {
+				int pos = buckets.FindFile_CmpCase(path_.GetLastSegment());
+				if (pos != -1) {
+					bucket_ = *buckets[pos].ownerGroup;
+					LogMessage(MessageType::Debug_Info, L"Directory is in bucket %s", bucket_);
+					opState = resolve_id;
+					return FZ_REPLY_CONTINUE;
+				}
+				else {
+					LogMessage(MessageType::Error, _("Bucket not found"));
+					return FZ_REPLY_ERROR;
+				}
+			}
+
+			opState = resolve_waitlistbuckets;
+			controlSocket_.List(CServerPath(L"/"), std::wstring(), 0);
+
+			return FZ_REPLY_CONTINUE;
+		}
+		break;
+	case resolve_id:
+		if (!fileId_) {
+			return FZ_REPLY_OK;
+		}
+		// FIXME
+		break;
+	}
+
+	LogMessage(MessageType::Debug_Warning, L"Unknown opState in CStorjResolveOpData::ResolveSend()");
+	return FZ_REPLY_INTERNALERROR;
+}
+
+int CStorjResolveOpData::ParseResponse()
+{
+	LogMessage(MessageType::Debug_Verbose, L"CStorjResolveOpData::ParseResponse() in state %d", opState);
+	return FZ_REPLY_INTERNALERROR;
+}
+
+int CStorjResolveOpData::SubcommandResult(int prevResult, COpData const& previousOperation)
+{
+	LogMessage(MessageType::Debug_Verbose, L"CStorjResolveOpData::SubcommandResult() in state %d", opState);
+
+	if (prevResult != FZ_REPLY_OK) {
+		return prevResult;
+	}
+
+	switch (opState) {
+	case resolve_waitlistbuckets:
+		{
+			CDirectoryListing buckets;
+
+			bool outdated{};
+			bool found = engine_.GetDirectoryCache().Lookup(buckets, currentServer_, CServerPath(L"/"), false, outdated);
+			if (found && !outdated) {
+				int pos = buckets.FindFile_CmpCase(path_.GetLastSegment());
+				if (pos != -1) {
+					bucket_ = *buckets[pos].ownerGroup;
+					LogMessage(MessageType::Debug_Info, L"Directory is in bucket %s", bucket_);
+					opState = resolve_id;
+					return FZ_REPLY_CONTINUE;
+				}
+			}
+		}
+		LogMessage(MessageType::Error, _("Bucket not found"));
+		return FZ_REPLY_ERROR;
+	}
+
+	LogMessage(MessageType::Debug_Warning, L"Unknown opState in CStorjResolveOpData::SubcommandResult()");
+	return FZ_REPLY_INTERNALERROR;
+}
---
+++ src/engine/storj/resolve.h
@@ -0,0 +1,30 @@
+#ifndef FILEZILLA_ENGINE_STORJ_RESOLVE_HEADER
+#define FILEZILLA_ENGINE_STORJ_RESOLVE_HEADER
+
+#include "storjcontrolsocket.h"
+
+class CStorjResolveOpData final : public COpData, public CStorjOpData
+{
+public:
+	CStorjResolveOpData(CStorjControlSocket & controlSocket, CServerPath const& path, std::wstring const& file, std::wstring & bucket, std::wstring * fileId = 0)
+		: COpData(PrivCommand::resolve)
+		, CStorjOpData(controlSocket)
+		, path_(path)
+		, file_(file)
+		, bucket_(bucket)
+		, fileId_(fileId)
+	{
+	}
+
+	virtual int Send() override;
+	virtual int ParseResponse() override;
+	virtual int SubcommandResult(int prevResult, COpData const& previousOperation) override;
+
+private:
+	CServerPath const path_;
+	std::wstring const file_;
+
+	std::wstring & bucket_;
+	std::wstring * const fileId_;
+};
+#endif
