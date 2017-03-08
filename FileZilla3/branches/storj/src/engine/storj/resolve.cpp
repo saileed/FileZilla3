fac21c6892892ef1c092807424863f63d9bb631d
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2017-03-08 11:25:03 +0000

    Implement deleting files.

--- FileZilla3/branches/storj/src/engine/storj/resolve.cpp
+++ FileZilla3/branches/storj/src/engine/storj/resolve.cpp
@@ -159,3 +159,143 @@ int CStorjResolveOpData::SubcommandResult(int prevResult, COpData const&)
 	LogMessage(MessageType::Debug_Warning, L"Unknown opState in CStorjResolveOpData::SubcommandResult()");
 	return FZ_REPLY_INTERNALERROR;
 }
+
+
+
+
+
+int CStorjResolveManyOpData::Send()
+{
+	LogMessage(MessageType::Debug_Verbose, L"CStorjResolveManyOpData::Send() in state %d", opState);
+
+	switch (opState) {
+	case resolve_init:
+		bucket_.clear();
+		fileIds_.clear();
+
+		if (path_.empty() || files_.empty()) {
+			return FZ_REPLY_INTERNALERROR;
+		}
+		else if (!path_.SegmentCount()) {
+			// It's the root, nothing to resolve here.
+			return FZ_REPLY_INTERNALERROR;
+		}
+		else if (path_.SegmentCount() != 1) {
+			return FZ_REPLY_INTERNALERROR;
+		}
+		else {
+			CDirectoryListing buckets;
+
+			bool outdated{};
+			bool found = engine_.GetDirectoryCache().Lookup(buckets, currentServer_, CServerPath(L"/"), false, outdated);
+			if (found && !outdated) {
+				int pos = buckets.FindFile_CmpCase(path_.GetLastSegment());
+				if (pos != -1) {
+					bucket_ = *buckets[pos].ownerGroup;
+					LogMessage(MessageType::Debug_Info, L"Directory is in bucket %s", bucket_);
+					opState = resolve_id;
+					return FZ_REPLY_CONTINUE;
+				}
+				else {
+					LogMessage(MessageType::Error, _("Bucket not found"));
+					return FZ_REPLY_ERROR;
+				}
+			}
+
+			opState = resolve_waitlistbuckets;
+			controlSocket_.List(CServerPath(L"/"), std::wstring(), 0);
+
+			return FZ_REPLY_CONTINUE;
+		}
+		break;
+	case resolve_id:
+		{
+			CDirectoryListing listing;
+
+			bool outdated{};
+			bool found = engine_.GetDirectoryCache().Lookup(listing, currentServer_, path_, false, outdated);
+			if (found && !outdated) {
+				for (auto const& file : files_ ) {
+					int pos = listing.FindFile_CmpCase(file);
+					if (pos != -1) {
+						LogMessage(MessageType::Debug_Info, L"File %s has id %s", path_.FormatFilename(file), *listing[pos].ownerGroup);
+						fileIds_.emplace_back(*listing[pos].ownerGroup);
+					}
+					else {
+						fileIds_.emplace_back();
+					}
+				}
+				return FZ_REPLY_OK;
+			}
+
+			opState = resolve_waitlist;
+			controlSocket_.List(path_, std::wstring(), 0);
+			return FZ_REPLY_CONTINUE;
+		}
+		break;
+	}
+
+	LogMessage(MessageType::Debug_Warning, L"Unknown opState in CStorjResolveManyOpData::ResolveSend()");
+	return FZ_REPLY_INTERNALERROR;
+}
+
+int CStorjResolveManyOpData::ParseResponse()
+{
+	LogMessage(MessageType::Debug_Verbose, L"CStorjResolveManyOpData::ParseResponse() in state %d", opState);
+	return FZ_REPLY_INTERNALERROR;
+}
+
+int CStorjResolveManyOpData::SubcommandResult(int prevResult, COpData const&)
+{
+	LogMessage(MessageType::Debug_Verbose, L"CStorjResolveManyOpData::SubcommandResult() in state %d", opState);
+
+	if (prevResult != FZ_REPLY_OK) {
+		return prevResult;
+	}
+
+	switch (opState) {
+	case resolve_waitlistbuckets:
+		{
+			CDirectoryListing buckets;
+
+			bool outdated{};
+			bool found = engine_.GetDirectoryCache().Lookup(buckets, currentServer_, CServerPath(L"/"), false, outdated);
+			if (found && !outdated) {
+				int pos = buckets.FindFile_CmpCase(path_.GetLastSegment());
+				if (pos != -1) {
+					bucket_ = *buckets[pos].ownerGroup;
+					LogMessage(MessageType::Debug_Info, L"Directory is in bucket %s", bucket_);
+					opState = resolve_id;
+					return FZ_REPLY_CONTINUE;
+				}
+			}
+		}
+		LogMessage(MessageType::Error, _("Bucket not found"));
+		return FZ_REPLY_ERROR;
+	case resolve_waitlist:
+		{
+			CDirectoryListing listing;
+
+			bool outdated{};
+			bool found = engine_.GetDirectoryCache().Lookup(listing, currentServer_, path_, false, outdated);
+			if (found && !outdated) {
+				for (auto const& file : files_ ) {
+					int pos = listing.FindFile_CmpCase(file);
+					if (pos != -1) {
+						LogMessage(MessageType::Debug_Info, L"File %s has id %s", path_.FormatFilename(file), *listing[pos].ownerGroup);
+						fileIds_.emplace_back(*listing[pos].ownerGroup);
+					}
+					else {
+						fileIds_.emplace_back();
+					}
+				}
+				return FZ_REPLY_OK;
+			}
+		}
+		LogMessage(MessageType::Error, _("Files not found"));
+		return FZ_REPLY_ERROR;
+	}
+
+	LogMessage(MessageType::Debug_Warning, L"Unknown opState in CStorjResolveManyOpData::SubcommandResult()");
+	return FZ_REPLY_INTERNALERROR;
+}
