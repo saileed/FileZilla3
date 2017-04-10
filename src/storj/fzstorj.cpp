61df5b4a949ec2e9f5fa72036ee0ad51f503528c
codesquid <codesquid@ce1b9ea0-41a8-11dc-9b12-cb80f3b22977>
2017-04-10 12:06:11 +0000

    Fix typo.

--- src/storj/fzstorj.cpp
+++ src/storj/fzstorj.cpp
@@ -102,7 +102,7 @@ extern "C" void get_buckets_callback(uv_work_t *work_req, int status)
 			}
 
 			if (encrypted) {
-				fzprintf(storjEvent::ErrorMsg, "Wrong encryption key for at lease one bucket");
+				fzprintf(storjEvent::ErrorMsg, "Wrong encryption key for at least one bucket");
 			}
 			fzprintf(storjEvent::Done);
 		}
